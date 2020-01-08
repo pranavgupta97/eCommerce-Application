f8e8c0595ca0512d9c103e2bb7a047ae8a51abf5
Pranav Gupta <pranavgupta9797@gmail.com>
2020-01-07 20:48:24 -0600

    Added TestUtils class for testing

---
+++ src/test/java/com/example/demo/TestUtils.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import java.lang.reflect.Field;
+
+public class TestUtils {
+    public static void injectObjects(Object target, String fieldName, Object toInject) {
+        boolean wasPrivate = false;
+
+        try {
+            Field field = target.getClass().getDeclaredField(fieldName);
+            if (!field.isAccessible()) {
+                field.setAccessible(true);
+                wasPrivate = true;
+            }
+            field.set(target, toInject);
+
+            if (wasPrivate) {
+                field.setAccessible(false);
+            }
+
+
+        } catch (NoSuchFieldException exception) {
+            exception.printStackTrace();
+        } catch (IllegalAccessException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
