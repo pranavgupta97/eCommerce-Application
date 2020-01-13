405033e16d7780c0c0149720e445a519b81e1a5c
Pranav Gupta <pranavgupta9797@gmail.com>
2020-01-12 21:01:05 -0600

    Added splunk dependency and Splunk Integration

--- src/main/java/com/example/demo/SareetaApplication.java
+++ src/main/java/com/example/demo/SareetaApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.splunk.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -12,6 +13,22 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EntityScan("com.example.demo.model.persistence")
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class SareetaApplication {
+
+    @Bean
+    public Service splunkService() {
+
+        Service.setSslSecurityProtocol(SSLSecurityProtocol.TLSv1_2);
+        ServiceArgs loginArguments = new ServiceArgs();
+        loginArguments.setUsername("pranavgupta97");
+        loginArguments.setPassword("Awesome123$$");
+        loginArguments.setScheme("https");
+        loginArguments.setHost("localhost");
+        loginArguments.setPort(8089);
+
+        Service service = Service.connect(loginArguments);
+        return service;
+    }
+
 
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
